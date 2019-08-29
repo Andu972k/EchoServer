5389450bf0d7da06531405576f775e8d0c4542ce
Anders Genderskov Binder <ande972k@edu.easj.dk>
2019-08-29 11:01:52 +0200

    Add project files.

---
+++ EchoServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EchoServer
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            
+            Server server = new Server();
+            server.Start();
+
+
+        }
+    }
+}
---
+++ EchoServer/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+    class Server
+    {
+
+
+
+
+
+
+
+
+        #region Methods
+
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 7);
+            server.Start();
+
+            while (true)
+            {
+                TcpClient socket = server.AcceptTcpClient();
+                Stream ns = socket.GetStream();
+                using (StreamReader sr = new StreamReader(ns))
+                using (StreamWriter sw = new StreamWriter(ns))
+                {
+                    string line = sr.ReadLine();
+                    sw.WriteLine(line);
+                    sw.Flush();
+
+                }
+                
+            }
+
+            
+        }
+
+        #endregion
+
+    }
+}
